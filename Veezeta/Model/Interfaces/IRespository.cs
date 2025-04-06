92faf7e71a96cdc5e5d81f17e16a17cb2da4092c
anas awaad <anas.shaban.awaad@gmail.com>
2025-04-06 16:46:30 +0200

    change folder structure

---
+++ Veezeta/Model/Interfaces/IRespository.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+
+namespace Vezeeta.Entities.Interfaces
+{
+    public interface IRespository<T> where T : class
+    {
+        IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null, string? properties = null);
+        T Get(Expression<Func<T, bool>> filter, string? properties = null, bool track = false);
+
+        void Add(T entity);
+        void Update(T entity);
+
+        void Remove(T entity);
+
+        void RemoveRange(IEnumerable<T> entity);
+    }
+}
---
+++ Veezeta/Model/ViewModel/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Vezeeta.Entities.ViewModel
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
